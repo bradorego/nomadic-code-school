04a0f75d0b6e1152e13dd87135a1b29f00cbd758
Brad Orego <me@bradorego.com>
2016-07-05 18:35:42 -0500

    add offset; change easing

--- js/grayscale.js
+++ js/grayscale.js
@@ -23,8 +23,8 @@
     $('a.page-scroll').bind('click', function(event) {
       var $anchor = $(this);
       $('html, body').stop().animate({
-        scrollTop: $($anchor.attr('href')).offset().top
-      }, 1500, 'easeInOutExpo');
+        scrollTop: $($anchor.attr('href')).offset().top - 50
+      }, 1500, 'easeOutExpo');
       event.preventDefault();
     });
   });
